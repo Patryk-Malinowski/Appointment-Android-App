6ac1e9dd71b5866ff44ac5b8ea7881242cd33d1a
Patryk Malinowski R00210173 <patryk.malinowski@mycit.ie>
2023-11-23 16:51:03 +0000

    Added a datepicker button using MaterialDatePicker.

--- app/src/main/java/com/example/assignment2/BookAppointmentActivity.java
+++ app/src/main/java/com/example/assignment2/BookAppointmentActivity.java
@@ -3,12 +3,49 @@ package com.example.assignment2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class BookAppointmentActivity extends AppCompatActivity {
+    private TextView selectedDateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_appointment);
+
+        selectedDateTextView = findViewById(R.id.selectedDateTextView);
+        Button pickDateButton = findViewById(R.id.pickDateButton);
+
+        pickDateButton.setOnClickListener(v -> showDatePicker());
+    }
+
+    private void showDatePicker() {
+        MaterialDatePicker<Long> datePicker = MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select Date")
+                .build();
+
+        datePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
+            @Override
+            public void onPositiveButtonClick(Long selectedDate) {
+                displaySelectedDate(selectedDate);
+            }
+        });
+
+        datePicker.show(getSupportFragmentManager(), datePicker.toString());
+    }
+
+    private void displaySelectedDate(Long selectedDate) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        String formattedDate = sdf.format(new Date(selectedDate));
+
+        selectedDateTextView.setText("Selected Date: " + formattedDate);
     }
 }
