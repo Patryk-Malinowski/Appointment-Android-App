4d5204e4c09e2a649ff9d28e4f1a436dd662be47
Patryk Malinowski R00210173 <patryk.malinowski@mycit.ie>
2023-11-23 16:45:04 +0000

    Completely redid the layout for the application.

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -1,54 +1,37 @@
 package com.example.assignment2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.CalendarView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-    Button continueButton;
-    CalendarView calendarView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        continueButton = findViewById(R.id.button);
-        calendarView = findViewById(R.id.calendarView);
+        Button bookAppointmentButton = findViewById(R.id.bookAppointmentButton);
+        Button viewAppointmentsButton = findViewById(R.id.viewAppointmentsButton);
 
-        // get the current date
-        long currentDate = System.currentTimeMillis();
-
-        // set the minimum date to today
-        calendarView.setMinDate(currentDate);
-
-        // set the maximum date to 2 months ahead
-        Calendar twoMonthsAhead = Calendar.getInstance();
-        twoMonthsAhead.add(Calendar.MONTH, 2);
-        long maxDate = twoMonthsAhead.getTimeInMillis();
-        calendarView.setMaxDate(maxDate);
-
-
-        // calendarView listener
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-
-            }
+        bookAppointmentButton.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, BookAppointmentActivity.class));
         });
 
-
-
-
-
-
+        viewAppointmentsButton.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, ViewAppointmentActivity.class));
+        });
     }
-
-
 }
 
 
+
+
+
+}
---
+++ app/src/main/java/com/example/assignment2/RecyclerViewActivity.java
@@ -0,0 +1,36 @@
+package com.example.assignment2;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private TextView dateTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+
+        recyclerView = findViewById(R.id.recyclerView1);
+        dateTextView = findViewById(R.id.dateTextView);
+
+        // Retrieve the selected date from the intent
+        long selectedDate = getIntent().getLongExtra("selectedDate", 0);
+
+        // Use the selectedDate as needed, for example, display it in a TextView
+        dateTextView.setText("Selected Date: " + selectedDate);
+
+        // Set up your RecyclerView as needed
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // Create an adapter and set it to the RecyclerView
+        // ExampleAdapter adapter = new ExampleAdapter(data); // Replace with your adapter
+        // recyclerView.setAdapter(adapter);
+    }
+}
