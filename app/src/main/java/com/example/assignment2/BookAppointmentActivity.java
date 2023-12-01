cd929ecb8f55e5749c9f23734dbf6a04b30476f4
Patryk Malinowski R00210173 <patryk.malinowski@mycit.ie>
2023-12-01 14:06:32 +0000

    Replaced calendar with TabLayout.

--- app/src/main/java/com/example/assignment2/BookAppointmentActivity.java
+++ app/src/main/java/com/example/assignment2/BookAppointmentActivity.java
@@ -1,17 +1,16 @@
-package com.example.assignment2;
+// Patryk Malinowski
+// R00210173
+// Assignment 2
 
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.assignment2;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.material.datepicker.CalendarConstraints;
-import com.google.android.material.datepicker.DateValidatorPointBackward;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.datepicker.MaterialDatePicker;
-import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.tabs.TabLayout;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -19,57 +18,67 @@ import java.util.Locale;
 
 public class BookAppointmentActivity extends AppCompatActivity {
     private final String TAG = "BookAppointmentActivity";
-    private TextView selectedDateTextView;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_appointment);
 
-        selectedDateTextView = findViewById(R.id.selectedDateTextView);
-        Button pickDateButton = findViewById(R.id.pickDateButton);
+        tabLayout = findViewById(R.id.tabLayout);
 
-        pickDateButton.setOnClickListener(v -> showDatePicker());
-    }
-
-    private void showDatePicker() {
-        long today = MaterialDatePicker.todayInUtcMilliseconds();
-        long fourteenDays = today + (14 * 24 * 60 * 60 * 1000);
+        addTabs();
 
 
-        CalendarConstraints.Builder constraintsBuilder = new CalendarConstraints.Builder();
-        constraintsBuilder.setValidator(DateValidatorPointBackward.before(fourteenDays));
-
-
-
-        MaterialDatePicker<Long> datePicker = MaterialDatePicker.Builder.datePicker()
-                .setTitleText("Select Date")
-                .setSelection(today) // Default selection is today
-                .setCalendarConstraints(constraintsBuilder.build())
-                .build();
-
-        datePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onPositiveButtonClick(Long selectedDate) {
-                if (selectedDate < today) {
-                    Toast.makeText(BookAppointmentActivity.this, "Date is in the past", Toast.LENGTH_SHORT).show();
-                    Log.e(TAG, "Date is in the past.");
-                }
-                else {
-                    displaySelectedDate(selectedDate);
-                }
+            public void onTabSelected(TabLayout.Tab tab) {
+                showDataForSelectedDate(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
             }
         });
-
-        datePicker.show(getSupportFragmentManager(), datePicker.toString());
     }
 
-    private void displaySelectedDate(Long selectedDate) {
+    // Method to display data for the selected date in tab layout
+    private void showDataForSelectedDate(int position) {
+        long selectedDate = calculateSelectedDate(position);
+
+        // Log the selected date
+        Log.d(TAG, convertDate(selectedDate));
+    }
+
+    // Method to add 14 tabs into tab layout (dates start from today)
+    private void addTabs() {
+        long today = MaterialDatePicker.todayInUtcMilliseconds();
+        long oneDay = (24 * 60 * 60 * 1000);
+        for (int i = 0; i < 14; i++) {
+
+            tabLayout.addTab(tabLayout.newTab().setText(convertDate(today)));
+            today += oneDay;
+        }
+    }
+
+    // Method to convert the date from milliseconds to dd/mm/yyyy
+    private String convertDate(long selectedDate) {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
-        String formattedDate = sdf.format(new Date(selectedDate));
-
-        selectedDateTextView.setText("Selected Date: " + formattedDate);
+        return sdf.format(new Date(selectedDate));
     }
+
+    // Method to calculate selected date
+    private long calculateSelectedDate(int position) {
+        long today = MaterialDatePicker.todayInUtcMilliseconds();
+        // Calculate the selected date based on the position (in days)
+        long selectedDate = today + (position * 24 * 60 * 60 * 1000);
+        return selectedDate;
+    }
+
 
 
 }
