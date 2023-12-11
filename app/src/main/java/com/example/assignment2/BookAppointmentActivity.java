7d224a139c7f775cc130812dc69926de69223c06
Patryk Malinowski R00210173 <patryk.malinowski@mycit.ie>
2023-12-11 23:07:44 +0000

    Fixed some layout issues in ViewAppointmentsByDateActivity.

--- app/src/main/java/com/example/assignment2/BookAppointmentActivity.java
+++ app/src/main/java/com/example/assignment2/BookAppointmentActivity.java
@@ -25,7 +25,6 @@ import java.util.Date;
 import java.util.Locale;
 
 public class BookAppointmentActivity extends AppCompatActivity {
-    private final String TAG = "BookAppointmentActivity";
     private TabLayout tabLayout;
     private MyRecyclerViewAdapter demo_adapter;
     private String selectedDate = "";
